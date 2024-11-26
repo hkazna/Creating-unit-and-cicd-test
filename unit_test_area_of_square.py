f06f30e4b95f7b330e20bddde8686f4f2d379734
Ivan Khazov <khazovitmo@mail.ru>
2024-11-26 21:22:54 +0300

    added the main file

---
+++ unit_test_area_of_square.py
@@ -0,0 +1,23 @@
+import unittest
+from unittest import*
+
+def area_square_on_the_coordinate_system(a):
+    return a * a
+
+
+class SquareTestCase(unittest.TestCase):
+    def test_area_of_square_with_positive_side(self):
+        result = area_square_on_the_coordinate_system(10)
+        self.assertEqual(result, 100)
+
+    def test_area_of_square_with_zero_side(self):
+        result = area_square_on_the_coordinate_system(0)
+        self.assertEqual(result, 0)
+
+    def test_area_of_square_with_negative_numbers(self):
+        result = area_square_on_the_coordinate_system(-10)
+        self.assertEqual(result, 100)
+
+#command for running unittests:  python.exe -m unittest square.py
+
+
