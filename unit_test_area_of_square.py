2c69432b92cda1828aaeac95e9da8ff7b6017277
Ivan Khazov <khazovitmo@mail.ru>
2024-11-28 08:48:04 +0300

    update

--- unit_test_area_of_square.py
+++ unit_test_area_of_square.py
@@ -18,6 +18,10 @@ class SquareTestCase(unittest.TestCase):
         result = area_square_on_the_coordinate_system(-10)
         self.assertEqual(result, 100)
 
+    def test_area_of_square_with_negative_numbers(self):
+        result = area_square_on_the_coordinate_system(0)
+        self.assertEqual(result, 100)
+
 #command for running unittests:  python.exe -m unittest square.py
 
 
